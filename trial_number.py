557f6831964159e54b7f11d03527424017878d29
Sean Chen <iamheat2458@hotmail.com>
2024-03-10 15:09:43 +0800

    2nd version with counting

--- trial_number.py
+++ trial_number.py
@@ -1,12 +1,16 @@
 import random
 number = random.randint(1, 100)
+count = 0
 while True: 
+    count = count + 1 
     guess = input('guess the number: ')
     guess = int(guess)
     if guess == number:
         print('you are right')
+        print('it is your', count, 'times')
         break 
     elif guess > number:
         print('too big')
     elif guess < number:
         print('to small')
+    print('it is your', count, 'times')
